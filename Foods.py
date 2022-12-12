53e7e278c12fd82e3df04fce4d7e80faa6b8cfe7
21-09622 <113603567+DanielIsaaiahCueto@users.noreply.github.com>
2022-12-12 15:24:20 +0800

    Update Foods.py

--- Foods.py
+++ Foods.py
@@ -1,1 +1,17 @@
+import random 
 
+from Utilities import Util
+
+class Food:
+  
+  def __init__(self):
+    
+    x = random.randiant(0, (Util.GAME_WIDTH 
+        / Util.SPACE_SIZE) - 1) * Util.SPACE_SIZE
+    y = random.randiant(0, (Util.GAME_HEIGHT
+        / Util.SPACE_SIZE) - 1) * Util.SPACE_SIZE
+    
+    self.coordinates = [x, y ]
+    
+    Util.canvas.create_oval(x, y, x + Util.SPACE_SIZE, y + Util
+        .SPACE_SIZE, fill=Util.FOOD_COLOR, tag="food")

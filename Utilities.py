94158792f50294ea7c711da985676dad193b2c55
Kim William Sanchez <114321964+21-05298@users.noreply.github.com>
2022-12-12 15:13:29 +0800

    Update Utilities.py

--- Utilities.py
+++ Utilities.py
@@ -1,1 +1,15 @@
+from tkinter import Tk, Canvas
 
+class Util:
+
+    GAME_WIDTH = 600
+    GAME_HEIGHT = 600
+    SPEED = 150
+    SPACE_SIZE = 20
+    BODY_PARTS = 3
+    SNAKE_COLOR = "#FEDD59"
+    FOOD_COLOR = "#FF0000"
+    BACKGROUND_COLOR = "#000000"
+
+    window = Tk()
+    canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)

5f978cc1fba8427f4191d3d0b7b4a57a7254f4a1
Art Cedrick <113785017+Art-Cedrick@users.noreply.github.com>
2022-12-12 15:21:01 +0800

    Update Snakes.py

--- Snakes.py
+++ Snakes.py
@@ -1,1 +1,15 @@
+from Utilities import Util
 
+class Snakes:
+  
+  def __init__(self):
+    self.body_size = Util.BODY_PARTS
+    self.coordinates = []
+    self.squares = []
+    
+    for i in range(0, Util.BODY_PARTS):
+      self.coordinates.append([0, 0])
+      
+    for x, y in self.coordinates:
+      square = Util.canvas.create_rectangle(x, y, x + Util.SPACE_SIZE, y + Util.SPACE_SIZE, fill=Util.SNAKE_COLOR, tag="snake")
+      self.squares.append(square)
